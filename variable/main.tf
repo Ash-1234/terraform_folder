77384e12312e2c3792192b223b5f39897ff970aa
Ash-1234 <32233218+Ash-1234@users.noreply.github.com>
2024-10-20 20:24:45 +0530

    new_key

--- variable/main.tf
+++ variable/main.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_instance" "ec2_type_1" {
     ami = local.instance_ami
     instance_type = "t2.micro"
-    key_name = "k8s"
+    key_name = "new_key_pair"
     tags = {
         Name = "ashwini"
     }
